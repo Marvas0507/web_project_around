8d1c9f8a851e80fc35f1b33cfa31a5fc832487c9
Marvin Vásquez <12davidvas@gmail.com>
2024-04-30 16:26:21 -0500

    poyect-5

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Tripleten web_project_around
+Es un sitio web en donde podras compartir las fotos de tu viaje al rededor de E.E.U.U., podrás darle like a cada imagen, modificar tu perfil e incluso agregar nuevas imagenes
+
+## Características del proyecto
+
+- HTML5 semántico
+- Display Flex
+- Posicionamiento
+- @font-face
+- Media queries
+- JavaScript y sus propiedades
+
+https://marvas0507.github.io/web_project_around/
---
+++ script.js
@@ -0,0 +1,30 @@
+const popupProfile = document.querySelector("#popup-profile");
+const profileButton = document.querySelector(".profile__info-content-add-button");
+
+const profileName = document.querySelector(".profile__info-content-name");
+const profileAbout = document.querySelector(".profile__info-paragraph");
+const inputName = document.querySelector("#input-name");
+const inputAbout = document.querySelector("#input-about");
+const formProfile = document.querySelector("#form-profile")
+
+const formClose = document.querySelector(".popup__button-close")
+
+profileButton.addEventListener("click", function(){
+    popupProfile.classList.add("popup__show");
+});
+
+function handleCloseProfile(){
+    popupProfile.classList.remove("popup__show");
+}
+
+formClose.addEventListener("click", function(){
+    popupProfile.classList.remove("popup__show");
+})
+
+formProfile.addEventListener("submit", function(){
+    evt.preventDefault();
+    profileName.textContent = inputName.value;
+    profileAbout.textContent = inputAbout.value;
+    handleCloseProfile();
+});
+
