ae1c63a0b017921177bd9f9b3d11d163ad09357c
Marvin Vásquez <12davidvas@gmail.com>
2024-12-07 17:16:02 -0500

    Components Api.js

---
+++ src/components/Api.js
@@ -0,0 +1,153 @@
+export default class Api {
+  constructor({ baseUrl, headers }) {
+    this._baseUrl = baseUrl;
+    this._headers = headers;
+  }
+
+  // Obtener información del usuario
+  getUserInfo() {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "GET",
+      headers: this._headers,
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // Obtener las tarjetas iniciales
+  getInitialCards() {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "GET",
+      headers: this._headers,
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // Actualizar perfil del usuario
+  updateUserProfile(data) {
+    return fetch(`${this._baseUrl}/users/me`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.name,
+        about: data.about,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // Añadir una nueva tarjeta
+  addNewCard(data) {
+    return fetch(`${this._baseUrl}/cards`, {
+      method: "POST",
+      headers: this._headers,
+      body: JSON.stringify({
+        name: data.name,
+        link: data.link,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // Dar "me gusta" a una tarjeta
+  likeCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      method: "PUT",
+      headers: this._headers,
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // Quitar "me gusta" de una tarjeta
+  dislikeCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // Eliminar una tarjeta
+  deleteCard(cardId) {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
+      method: "DELETE",
+      headers: this._headers,
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  // Método para actualizar el avatar del usuario
+  updateUserAvatar(data) {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
+      method: "PATCH",
+      headers: this._headers,
+      body: JSON.stringify({
+        avatar: data.avatarLink,
+      }),
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        return Promise.reject(`Error: ${res.status}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,43 @@
-import './index.css';
+import "./index.css";
 import Card from "../components/Card.js";
 import FormValidator from "../components/FormValidator.js";
 import PopupWithForm from "../components/PopupWithForms.js";
 import PopupWithImage from "../components/PopupWithImage.js";
-import Section from '../components/Section.js';
+import Section from "../components/Section.js";
 
 const formProfile = document.querySelector("#form-profile");
 const formClose = document.querySelector("#form-button-close");
 const profileName = document.querySelector(".profile__info-content-name");
 const profileAbout = document.querySelector(".profile__info-paragraph");
-const profileButton = document.querySelector(".profile__info-content-add-button");
+const profileButton = document.querySelector(
+  ".profile__info-content-add-button"
+);
 
 const initialCards = [
-    {
-        name: "Valle de Yosemite",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg"
-    },
-    {
-        name: "Lago Louise",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg"
-    },
-    {
-        name: "Montañas Calvas",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg"
-    },
-    {
-        name: "Latemar",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg"
-    },
-    {
-        name: "Parque Nacional de la Vanoise",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg"
-    },
-    {
-        name: "Lago di Braies",
-        link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg"
-    }
+  {
+    name: "Valle de Yosemite",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/yosemite.jpg",
+  },
+  {
+    name: "Lago Louise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lake-louise.jpg",
+  },
+  {
+    name: "Montañas Calvas",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/latemar.jpg",
+  },
+  {
+    name: "Parque Nacional de la Vanoise",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/new-markets/WEB_sprint_5/ES/lago.jpg",
+  },
 ];
 
 const cardArea = document.querySelector(".element");
@@ -44,70 +46,68 @@ const cardButton = document.querySelector(".profile__button-add");
 const cardClose = document.querySelector("#card-button-close");
 const buttonClosePopupPhoto = document.querySelector("#popup-photo-close");
 
-let initialCards = null;
+const section = new Section(
+  {
+    items: initialCards,
+    renderer: function (item) {
+      const initialCard = new Card(item.name, item.link, () => {
+        popupImage.open({ name: item.name, link: item.link });
+      }).createCard();
+      section.addItem(initialCard);
+    },
+  },
+  ".element"
+);
 
-fetch("https://around.nomoreparties.co/v1/web_es_17/cards",{
-    headers: {
-        authorization: "aca41053-935f-4980-ab7e-41af6eea4631"
-    }
-}).then
+section.renderItems();
 
-const section  = new Section({
-    items: initialCards, 
-    renderer: function (item){
-        const initialCard = new Card(item.name, item.link, () => {popupImage.open({name: item.name, link: item.link});} ).createCard();
-        section.addItem(initialCard);
-    }
-}, 
-'.element');
-
-section.renderItems();    
-
-const popupProfile = new PopupWithForm ("#popup-profile", (inputs) => {
-    profileName.textContent = inputs.name;
-    profileAbout.textContent = inputs.about;
+const popupProfile = new PopupWithForm("#popup-profile", (inputs) => {
+  profileName.textContent = inputs.name;
+  profileAbout.textContent = inputs.about;
 });
 
 popupProfile.setEventListeners();
 
-const popupCards = new PopupWithForm ("#popup-cards", (inputs) => {
-    const initialCard = new Card(inputs.title, inputs.link, ()=> { popupImage.open({ name: inputs.title, link: inputs.link });}).createCard();
-    cardArea.prepend(initialCard);
+const popupCards = new PopupWithForm("#popup-cards", (inputs) => {
+  const initialCard = new Card(inputs.title, inputs.link, () => {
+    popupImage.open({ name: inputs.title, link: inputs.link });
+  }).createCard();
+  cardArea.prepend(initialCard);
 });
 
 popupCards.setEventListeners();
 
-const popupImage = new PopupWithImage ("#popup-photo");
+const popupImage = new PopupWithImage("#popup-photo");
 
 profileButton.addEventListener("click", () => {
-    popupProfile.open();
+  popupProfile.open();
 });
 
 cardButton.addEventListener("click", () => {
-    popupCards.open();
+  popupCards.open();
 });
 
 formClose.addEventListener("click", () => {
-    popupProfile.close();
+  popupProfile.close();
 });
 
 cardClose.addEventListener("click", () => {
-    popupCards.close();
+  popupCards.close();
 });
 
 buttonClosePopupPhoto.addEventListener("click", () => {
-    popupCards.close();
+  popupCards.close();
 });
 
-popupImage.setEventListeners( );
+popupImage.setEventListeners();
 
 const config = {
-    formSelector: ".form",
-    inputSelector: ".form__content-input",
-    submitButtonSelector: ".form__button",
-    inactiveButtonClass: "form__button_disabled",
-    inputErrorClass: "form__content-input_type_error",
-    inputErrorMessageClass: "form__error-message",
+  formSelector: ".form",
+  inputSelector: ".form__content-input",
+  submitButtonSelector: ".form__button",
+  inactiveButtonClass: "form__button_disabled",
+  inputErrorClass: "form__content-input_type_error",
+  inputErrorMessageClass: "form__error-message",
 };
 
 const profileFormValidator = new FormValidator(config, formProfile);
